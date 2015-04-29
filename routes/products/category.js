b9b2db315b58ee95a8a8eb636ebb409e4f4baa63
刘阳光 <liuyangguang@ofcard.com>
2015-04-29 16:32:54 +0800

    二手哥api

--- routes/products/category.js
+++ routes/products/category.js
@@ -1,18 +1,38 @@
+/**
+ *  商品分类管理
+ *  @author youngguang
+ */
+
+
 var express = require('express');
 var router = express.Router();
 
 
-// 增
+// 查列表
+router.post('/cate/list', function (req, res, next) {
+
+})
 
 
+// 查单
+router.post('/cate/:id', function (req, res, next) {
 
-// 删
-
-
-
-// 改
+})
 
 
+// 增单
+router.post('/cate/add', function (req, res, next) {
+
+})
 
 
-// 查
+// 删批量
+router.post('/cate/del', function (req, res, next) {
+
+})
+
+
+// 改单
+router.post('/cate/update', function (req, res, next) {
+
+})
